3a2f02ce84cd7a7ceaa0a2f450feed9301407bc7
Bofei Zhu <bofei.zhu@healthtap.com>
2019-09-25 10:20:16 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["DangerXcodeStaticAnalyzer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"),
+        .package(url: "https://github.com/danger/swift.git", from: "2.0"),
     ],
     targets: [
         .target(
